3736a9e9cf694d00dc3eb9d6f53602fe7576dd85
Bart Veldstra <bart.veldstra@gmail.com>
2020-04-18 15:10:57 +0200

    Align all entities at half tiles and restart game if left

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    Player m_player;
+    BoxCollider2D m_collider;
+    float m_leftTime = 0f;
+
+    private void Awake()
+    {
+        m_collider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (!IsPlayerInCage())
+        {
+            m_leftTime += Time.deltaTime;
+            if (m_leftTime > 2f)
+            {
+                RestartLevel();
+            }
+        }
+    }
+
+    private bool IsPlayerInCage()
+    {
+        if (m_player == null)
+        {
+            m_player = FindObjectOfType<Player>();
+        }
+
+        return m_player && m_collider.OverlapPoint(m_player.transform.position);
+    }
+
+    private void RestartLevel()
+    {
+        var current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.buildIndex, LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/TileAligned.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileAligned : MonoBehaviour
+{
+    void Awake()
+    {
+        var pos = transform.position;
+        pos.x = Mathf.Round(pos.x * 2f) / 2f;
+        pos.y = Mathf.Round(pos.y * 2f) / 2f;
+        transform.position = pos;
+    }
+}
