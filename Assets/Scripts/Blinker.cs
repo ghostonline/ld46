5974369573aa61e21c0280c1b8b8832fa78e54f8
Bart Veldstra <bart.veldstra@gmail.com>
2020-04-19 12:12:01 +0200

    Add intro and outtro cards

---
+++ Assets/Scripts/Blinker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinker : MonoBehaviour
+{
+    public UnityEngine.UI.Text m_textRender;
+    public float m_blinkSpeed;
+
+    float m_timer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer >= m_blinkSpeed)
+        {
+            m_textRender.enabled = !m_textRender.enabled;
+            m_timer -= m_blinkSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/ClickToContinue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToContinue : MonoBehaviour
+{
+    public int m_forcedBuildIndex = -1;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            var buildIndexToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+            if (m_forcedBuildIndex > -1)
+            {
+                buildIndexToLoad = m_forcedBuildIndex;
+            }
+            SceneManager.LoadScene(buildIndexToLoad);
+        }
+    }
+}
